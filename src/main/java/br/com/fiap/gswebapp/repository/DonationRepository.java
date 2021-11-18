4c07de167eeefc4d19c18c6b332aad412789c359
giovanna <giovannacaroline135@gmail.com>
2021-11-18 10:10:16 -0300

    Remoção método inutilizado

--- src/main/java/br/com/fiap/gswebapp/repository/DonationRepository.java
+++ src/main/java/br/com/fiap/gswebapp/repository/DonationRepository.java
@@ -1,12 +1,7 @@
 package br.com.fiap.gswebapp.repository;
 
-import br.com.fiap.gswebapp.model.Advertising;
+import br.com.fiap.gswebapp.model.Donation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import br.com.fiap.gswebapp.model.Donation;
-
-import java.util.Optional;
-
 public interface DonationRepository extends JpaRepository<Donation, Long>{
-    public Optional<Donation> findDonationByAdvertising_Id(Long advertising_id);
 }
